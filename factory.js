9eab66572092781f686c1b528d1422b23b487cbd
damcipolat <damian.cipolat@gmail.com>
2017-11-26 14:18:27 -0300

    subo factory

---
+++ factory.js
@@ -0,0 +1,57 @@
+class person{
+
+    constructor(name,surname,age,sex){
+
+      this.name = name;
+      this.surname = surname;
+      this.age = age;
+      this.sex = sex;
+
+    }
+
+}
+
+class personFactory{
+
+   constructor(name,surname,age,sex){
+
+      this.name    = null;
+      this.surname = null;
+      this.age = null;
+      this.sex = null;
+
+    }
+
+    setName(name){
+        this.name = name;
+    }
+
+    setSurname(surname){
+        this.surname = surname;
+    }
+
+    setAge(age){
+        this.age = age;
+    }    
+
+    setSex(sex){
+        this.sex = sex;
+    }
+
+    build(){
+
+        return new person(this.name,this.surname,this.age,this.sex);
+
+    }
+
+}
+
+let fabrica = new personFactory();
+fabrica.setName('Damián');
+fabrica.setSurname('Cipolat');
+fabrica.setAge(30);
+fabrica.setSex('M');
+
+let damian = fabrica.build();
+
+console.log(damian);
