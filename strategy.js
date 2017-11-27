dd8af0cc0e98c1fe67eff1449bf97ce9eab5a328
damcipolat <damian.cipolat@gmail.com>
2017-11-27 00:08:36 -0300

    upd strat

--- strategy.js
+++ strategy.js
@@ -46,3 +46,35 @@ mathTest.make();
 
 mathTest.setAlumn(new alumnHard());
 mathTest.make();
+
+/* EJemplo con busquedas */
+
+class person{
+
+  constructor(name,sex,age){
+    this.name = name;
+    this.sex  = sex;
+    this.age  = age;
+  }
+
+}
+
+let nameFilter = (list,val)=>{
+  list.filter((item)=>(item.name==val));
+}
+
+let sexFilter = (list,val)=>{
+  list.filter((item)=>(item.sex==val));
+}
+
+let ageFilter = (list,val)=>{
+  list.filter((item)=>(item.age==val));
+}
+
+let peoples = [new person('Damian','M',30),new person('Paul','M',40),new person('Michael','M',20),new person('Jennifer','F',50)];
+
+nameFinder(peoples,'Damian');
+
+sexFilter(peoples,'F');
+
+ageFilter(peoples,30);
