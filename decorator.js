16b8961001c426dd2e3bf984ac9a628619fc189a
damcipolat <damian.cipolat@gmail.com>
2017-11-26 22:39:33 -0300

    upd decorator

--- decorator.js
+++ decorator.js
@@ -1,33 +1,27 @@
-class person(){
+/* Ejemplo decorator */
 
+class person(){
   hello(){
     return 'hello world';
   }
-
   greetings(){
     return 'greetings people';
   }
-
 }
 
 class personDecorator{
-
   constructor(component){
     this.component = component;
   }
-
   hello(){
     return this.component.hello();
   }
-
   greetings(){
     return this.component.greetings();
   }
-
   jump(){
     return this.component.hello() + this.component.greetings();
   }
-
 }
 
 let damian      = new person();
@@ -37,3 +31,32 @@ damian.hello();
 damian.greetings();
 
 console.log(damian.jump());
+
+/* Ejemplo decorador para un log */
+
+const loggingDecorator = (wrapped)=>{
+
+  return ()=>{
+    console.log('Starting');
+    const result = wrapped.apply(this, arguments);
+    console.log('Finished');
+    return result;    
+  }
+
+}
+
+const doSomething = (name)=>{
+  console.log('Hello, ' + name);
+}
+
+
+const wrapped = loggingDecorator(doSomething);
+
+doSomething('Graham');
+// Hello, Graham
+
+wrapped('Graham');
+
+// Starting
+// Hello, Graham
+// Finished
