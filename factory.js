a86479896cd043701a7474149151876a8f3b7005
jairo quispe <joelquispeunju@gmail.com>
2020-08-06 22:13:27 -0300

    feature/minor-fixes:
    - Formateo y fixes de algunos fragmentos de codigo.
    - Algunas correcciones y agregados en la escritura del texto
    - Agregado de fragmento del libro para el patron Universal
    - Correcciones y formateo en los modulos factory.js y proxy.js

--- factory.js
+++ factory.js
@@ -1,60 +1,50 @@
 /*
   Ejemplo Fabrica de personas
 */
-class person{
-
-    constructor(name,surname,age,sex){
-
-      this.name = name;
-      this.surname = surname;
-      this.age = age;
-      this.sex = sex;
-
-    }
-
+class person {
+  constructor(name, surname, age, sex) {
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+    this.sex = sex;
+  }
 }
 
-class personFactory{
+class personFactory {
+  constructor(name, surname, age, sex) {
+    this.name = null;
+    this.surname = null;
+    this.age = null;
+    this.sex = null;
+  }
 
-   constructor(name,surname,age,sex){
+  setName(name) {
+    this.name = name;
+  }
 
-      this.name    = null;
-      this.surname = null;
-      this.age = null;
-      this.sex = null;
+  setSurname(surname) {
+    this.surname = surname;
+  }
 
-    }
+  setAge(age) {
+    this.age = age;
+  }
 
-    setName(name){
-        this.name = name;
-    }
+  setSex(sex) {
+    this.sex = sex;
+  }
 
-    setSurname(surname){
-        this.surname = surname;
-    }
-
-    setAge(age){
-        this.age = age;
-    }    
-
-    setSex(sex){
-        this.sex = sex;
-    }
-
-    build(){
-
-        return new person(this.name,this.surname,this.age,this.sex);
-
-    }
-
+  build() {
+    return new person(this.name, this.surname, this.age, this.sex);
+  }
 }
 
-//Invocación.
+// Invocación.
 let fabrica = new personFactory();
-fabrica.setName('Damián');
-fabrica.setSurname('Cipolat');
+fabrica.setName("Damián");
+fabrica.setSurname("Cipolat");
 fabrica.setAge(30);
-fabrica.setSex('M');
+fabrica.setSex("M");
 
 let damian = fabrica.build();
 
@@ -63,35 +53,33 @@ console.log(damian);
 /*
   Ejemplo fabrica de profilers.
 */
-class profiler{
-	
-	constructor(label){
-		this.label 	  = label;
-		this.lastTime = null;
-	}
-	
-	start(){
-		this.lastTime = process.hrtime();
-	}
-	
-	end(){
-		let dif = process.hrtime(this.lastTime);
-		
-		console.log('Timer "' + this.label + '" took '+ diff[0] + ' seconds and '+ diff[1] + ' nanoseconds.');	
-	}
+class profiler {
+  constructor(label) {
+    this.label = label;
+    this.lastTime = null;
+  }
+
+  start() {
+    this.lastTime = process.hrtime();
+  }
+
+  end() {
+    let diff = process.hrtime(this.lastTime);
+    console.log(
+      `Timer ${this.label} took ${diff[0]} seconds and ${diff[1]} nanoseconds.`
+    );
+  }
 }
 
-//Invocación.
-if(process.env.NODE_ENV === 'development')	
- return new Profiler(label);
-else{
-	
-	if(process.env.NODE_ENV === 'production')
-	 return {
-				start: function() {},
-				end: function() {}
-			}
-	else{
-		throw new Error('Must set NODE_ENV');
-	}
- }
+// Invocación.
+if (process.env.NODE_ENV === "development") return new Profiler(label);
+else {
+  if (process.env.NODE_ENV === "production")
+    return {
+      start: function () {},
+      end: function () {},
+    };
+  else {
+    throw new Error("Must set NODE_ENV");
+  }
+}
--- proxy.js
+++ proxy.js
@@ -1,129 +1,108 @@
 /*  Ejemplo base  */
-
-class subject{
-
-  hello(){
-    return 'hello';
+class subject {
+  hello() {
+    return "hello";
   }
 
-  goodbye(){
-    return 'Goodbye people!';
+  goodbye() {
+    return "Goodbye people!";
   }
-
 }
 
-class Proxy(subject){
-
-  constructor(subject){
+class Proxy {
+  constructor(subject) {
     this.subject = subject;
   }
 
-  hello(){
-    return this.subject.hello()+' world!';
+  hello() {
+    return this.subject.hello() + " world!";
   }
 
-  goodbye(){
+  goodbye() {
     return this.subject.goodbye();
   }
-
 }
 
 let tmpsubj = new subject();
-let proxy   = new Proxy(tmpsubj);
+let proxy = new Proxy(tmpsubj);
 
 console.log(proxy.hello());
 console.log(proxy.goodbye());
 
 /*  Ejemplo Data Validation */
-
-class calculator{
-
-  sumCalc(a,b){
-    return a+b;
+class calculator {
+  sumCalc(a, b) {
+    return a + b;
   }
 
-  difCalc(a,b){
-    return a-b;
+  diffCalc(a, b) {
+    return a - b;
   }
-
 }
 
-class validatorCalc{
-
-  constructor(subject){
+class validatorCalc {
+  constructor(subject) {
     this.subject = subject;
   }
 
-  sumar(numA,numB){
-
-    if ((typeof numA=="number")&&(typeof numB=="number"))
-      return this.subject.sumCalc(numA,numB);
-    else
-      throw "Parametros no numericos";
-
+  sumar(numA, numB) {
+    if (typeof numA == "number" && typeof numB == "number")
+      return this.subject.sumCalc(numA, numB);
+    else throw "Parámetros no numéricos";
   }
 
-  restar(numA,numB){
-
-    if ((typeof numA=="number")&&(typeof numB=="number"))
-      return this.subject.difCalc(numA,numB);
-    else
-      throw "Parametros no numericos";
-
+  restar(numA, numB) {
+    if (typeof numA == "number" && typeof numB == "number")
+      return this.subject.diffCalc(numA, numB);
+    else throw "Parámetros no numéricos";
   }
-
 }
 
 let calculator = new calculator();
-let validator  = new validatorCalc(calculator);
+let validator = new validatorCalc(calculator);
 
-console.log(validator.sumar(2,5));
-console.log(validator.restar(80,5));
-
+console.log(validator.sumar(2, 5));
+console.log(validator.restar(80, 5));
 
 /*  Ejemplo log  */
-
 function createLoggingWritable(writableOrig) {
+  var proto = Object.getPrototypeOf(writableOrig);
 
- var proto = Object.getPrototypeOf(writableOrig);
+  function LoggingWritable(subject) {
+    this.writableOrig = writableOrig;
+  }
 
- function LoggingWritable(subject){
-  this.writableOrig = writableOrig;
- }
+  LoggingWritable.prototype = Object.create(proto);
 
- LoggingWritable.prototype = Object.create(proto);
-
- LoggingWritable.prototype.write = function(chunk, encoding, callback) {
-
-    if(!callback && typeof encoding === 'function') {
+  LoggingWritable.prototype.write = function (chunk, encoding, callback) {
+    if (!callback && typeof encoding === "function") {
       callback = encoding;
       encoding = undefined;
     }
 
-    console.log('Writing ', chunk);
+    console.log("Writing ", chunk);
 
-    return this.writableOrig.write(chunk, encoding, function() {
-      console.log('Finished writing ', chunk);
+    return this.writableOrig.write(chunk, encoding, function () {
+      console.log("Finished writing ", chunk);
       callback && callback();
     });
- };
+  };
 
- LoggingWritable.prototype.on = function() {
-  return this.writableOrig.on.apply(this.writableOrig, arguments);
- };
+  LoggingWritable.prototype.on = function () {
+    return this.writableOrig.on.apply(this.writableOrig, arguments);
+  };
 
- LoggingWritable.prototype.end = function() {
-  return this.writableOrig.end.apply(this.writableOrig, arguments);
- }
+  LoggingWritable.prototype.end = function () {
+    return this.writableOrig.end.apply(this.writableOrig, arguments);
+  };
 
- return new LoggingWritable(this.writableOrig);
-
+  return new LoggingWritable(this.writableOrig);
 }
 
-var fs = require('fs');
-var writable = fs.createWriteStream('test.txt');
+var fs = require("fs");
+var writable = fs.createWriteStream("test.txt");
 var writableProxy = createProxy(writable);
-writableProxy.write('First chunk');
-writableProxy.write('Second chunk');
-writable.write('This is not logged');
+writableProxy.write("First chunk");
+writableProxy.write("Second chunk");
+writable.write("This is not logged");
 writableProxy.end();
